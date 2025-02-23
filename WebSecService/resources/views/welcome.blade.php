b2099d06d81023067e8bc8d1f07ec1e5aa03d758
YassinShaherr <yassine23103609@sut.edu.eg>
2025-02-24 00:19:45 +0200

    fixed the code for assignment one as I rewrote it

--- WebSecService/resources/views/welcome.blade.php
+++ WebSecService/resources/views/welcome.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.master')
-@section('title', 'Welcome')
+@section('title', 'Home')
 @section('content')
-    <div class="card m-4">
-        <div class="card-header">Welcome</div>
-        <div class="card-body">
-            Welcome to hme page
+    <h1 style="text-align: center">Welcome to WebSec</h1>
+    <div class="contiainer m-3">
+        <div class="card-body" style="text-align: center">
+            <button type="button" class="btn btn-primary" onclick="doSomething()">Press Me</button>
         </div>
+
     </div>
+    <script>
+        function doSomething() {
+            alert('Hello From JavaScript');
+        }
+    </script>
 @endsection
--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -4,17 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/multable', function () {
     return view('multable');
-});
+})->name('multable');
 
 Route::get('/even', function () {
-    return view('even');
-});
+    return view('even_number');
+})->name('even');
 
 Route::get('/prime', function () {
-    return view('prime');
-});
-
+    return view('prime_number');
+})->name('prime');
